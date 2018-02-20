8d45821a39512c8e23d4e6853f16dfcea1fbbf75
Jasper Roel <jasperroel@gmail.com>
2018-02-20 21:15:35 +0100

    Made some basic Java errors go away
    As reported by IntelliJ

--- src/main/java/com/yacht/BasicCrawler.java
+++ src/main/java/com/yacht/BasicCrawler.java
@@ -3,6 +3,7 @@ package com.yacht;
 import com.salesforce.webdev.sitecrawler.SiteCrawler;
 import com.salesforce.webdev.sitecrawler.SiteCrawlerAction;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class BasicCrawler {
@@ -10,8 +11,8 @@ public class BasicCrawler {
     public static void main(String[] args) {
         basic();
     }
-    public static void basic() {
-        List<? extends SiteCrawlerAction> siteCrawlerActions = null;
+    private static void basic() {
+        List<? extends SiteCrawlerAction> siteCrawlerActions = new LinkedList<>();
         SiteCrawler siteCrawler = new SiteCrawler(null, "https://www.ziggo.nl", siteCrawlerActions);
         siteCrawler.getAllowedSuffixes().add("");
         siteCrawler.navigate();
